7a0cf319dd5040e46f727912d8edbc489df89256
Selloston <aymankader539@gmail.com>
2024-08-25 23:49:39 +0100

    change
    Signed-off-by: Selloston <aymankader539@gmail.com>

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -16,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // الحصول على المثيلات المختلفة
+export {app}
 export const auth = getAuth(app);
-export const storage = getStorage(app); // إضافة تصدير Firebase Storage
+export const storage = getStorage(app);// إضافة تصدير Firebase Storage
