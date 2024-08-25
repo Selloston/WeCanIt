5f5e2276258615a135ca8c8a250ec5f2cdb4ba74
Selloston <aymankader539@gmail.com>
2024-08-26 00:13:20 +0100

    chang
    Signed-off-by: Selloston <aymankader539@gmail.com>

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,25 +1,20 @@
-'use client'
+'use client';
 
-import { useRef } from 'react'
-//import all assets
-import './page.css'
-import { useSearchParams } from 'next/navigation'
+import React from 'react';
 
-//The default function
-const page = ({searchParams} : {searchParams : {src: string}}) => {
+const VideoPage: React.FC = () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const src = searchParams.get("src");
 
-  let searchparams = useSearchParams();
-
-  return (
-    <>
-      <div className='contanervideo'>
-        <div  className='videorad'>
-          <video controls className='video' src={searchparams.get("src")} autoPlay></video>
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='contanervideo'>
+                <div className='videorad'>
+                    <video controls className='video' src={src ?? ''} autoPlay></video>
+                </div>
+            </div>
+        </>
+    );
 }
 
-//expotr the default function
-export default page;
+export default VideoPage;
